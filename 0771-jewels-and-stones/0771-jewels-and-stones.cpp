ca646e625b472cf42c74ffafddfb38d47dcf8e1f
sourabh1-1 <116716478+sourabh1-1@users.noreply.github.com>
2023-07-10 17:11:19 +0530

    Time: 5 ms (30.42%), Space: 6.3 MB (50.05%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int count=0;
+        for(int i=0;i<stones.length();i++){
+            if(jewels.find(stones.at(i))!=std::string::npos)
+                count++;
+        }
+        return count;
+    }
+};
