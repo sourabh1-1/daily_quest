dbf5cd9f39fb4a7da3370e1fa8dd8123ecfe2605
sourabh1-1 <116716478+sourabh1-1@users.noreply.github.com>
2023-07-15 16:01:50 +0530

    Time: 7 ms (22.73%), Space: 8.2 MB (33.02%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -5,15 +5,13 @@ public:
         int n2=b.length()-1;
         int c=0;
         string ans="";
-        while(n1>=0 || n2>=0){
+        while(n1>=0 || n2>=0 || c>0){
             int x = (n1 >= 0) ? a[n1--] - '0' : 0;
             int y = (n2 >= 0) ? b[n2--] - '0' : 0;
             int sum = x + y + c;
             ans=to_string((x+y+c)%2)+ans;
             c=(x+y+c)/2;
         }
-        if(c)
-            ans=to_string(c)+ans;
         return ans;
     }
 };
