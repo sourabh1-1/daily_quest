dda9c20f05556834da381e4aec162355455eb68e
sourabh1-1 <116716478+sourabh1-1@users.noreply.github.com>
2024-02-12 18:08:53 +0530

    Time: 24 ms (68.50%), Space: 94.6 MB (21.71%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         
-        //brute force
+//brute force
 //         double median;
 //         int n1=nums1.size();
 //         int n2=nums2.size();
@@ -20,50 +20,83 @@ public:
 //             median=merg[i];
 //         return median;
         
-        //better solution
+//better solution
+        // int n1=nums1.size(), n2=nums2.size();
+        // int n=n1+n2;
+        // int ind2=n/2;
+        // int ind1=ind2-1;
+        // int ind1el=-1, ind2el=-1;
+        // int count=0;
+        // int i=0, j=0;
+        // while(i<n1 && j<n2){
+        //     if(nums1[i]<nums2[j]){
+        //         if(count==ind1)
+        //             ind1el=nums1[i];
+        //         if(count==ind2)
+        //             ind2el=nums1[i];
+        //         i++;
+        //         count++;
+        //     }
+        //     else{
+        //         if(count==ind1)
+        //             ind1el=nums2[j];
+        //         if(count==ind2)
+        //             ind2el=nums2[j];
+        //         j++;
+        //         count++;
+        //     }
+        // }
+        // while(i<n1){
+        //     if(count==ind1)
+        //         ind1el=nums1[i];
+        //     if(count==ind2)
+        //         ind2el=nums1[i];
+        //     i++;
+        //     count++;
+        // }
+        // while(j<n2){
+        //     if(count==ind1)
+        //         ind1el=nums2[j];
+        //     if(count==ind2)
+        //         ind2el=nums2[j];
+        //     j++;
+        //     count++;
+        // }
+        // if(n%2==1)
+        //     return ind2el;
+        // return (double)(ind1el+ind2el)/2.0;
+        
+//optimal solution
         int n1=nums1.size(), n2=nums2.size();
+        if(n1>n2)
+            return findMedianSortedArrays(nums2, nums1);
+        int low=0;
+        int high=n1;
         int n=n1+n2;
-        int ind2=n/2;
-        int ind1=ind2-1;
-        int ind1el=-1, ind2el=-1;
-        int count=0;
-        int i=0, j=0;
-        while(i<n1 && j<n2){
-            if(nums1[i]<nums2[j]){
-                if(count==ind1)
-                    ind1el=nums1[i];
-                if(count==ind2)
-                    ind2el=nums1[i];
-                i++;
-                count++;
+        int left=(n1+n2+1)/2;
+        while(low<=high){
+            int mid1=(low+high)/2;
+            int mid2=left-mid1;
+            int l1=INT_MIN, l2=INT_MIN;
+            int r1=INT_MAX, r2=INT_MAX;
+            if(mid1-1>=0)
+                l1=nums1[mid1-1];
+            if(mid2-1>=0)
+                l2=nums2[mid2-1];
+            if(mid1<n1)
+                r1=nums1[mid1];
+            if(mid2<n2)
+                r2=nums2[mid2];
+            if(l1<=r2 && l2<=r1){
+                if(n%2==1)
+                    return max(l1,l2);
+                return ((double)(max(l1, l2)+min(r1, r2)))/2.0;
             }
-            else{
-                if(count==ind1)
-                    ind1el=nums2[j];
-                if(count==ind2)
-                    ind2el=nums2[j];
-                j++;
-                count++;
-            }
+            else if(l1>r2)
+                high=mid1-1;
+            else
+                low=mid1+1;
         }
-        while(i<n1){
-            if(count==ind1)
-                ind1el=nums1[i];
-            if(count==ind2)
-                ind2el=nums1[i];
-            i++;
-            count++;
-        }
-        while(j<n2){
-            if(count==ind1)
-                ind1el=nums2[j];
-            if(count==ind2)
-                ind2el=nums2[j];
-            j++;
-            count++;
-        }
-        if(n%2==1)
-            return ind2el;
-        return (double)(ind1el+ind2el)/2.0;
+        return 0;
     }
 };
