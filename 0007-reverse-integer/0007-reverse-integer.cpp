a19c4eda9e42fd0facce926404a43765279b2889
sourabh1-1 <116716478+sourabh1-1@users.noreply.github.com>
2024-07-19 23:40:14 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (5.97%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -6,11 +6,11 @@ public:
             x=abs(x);
             flag=1;
         }
-        long long temp=0;
+        long long int temp=0;
         while(x>0){
             int c=x%10;
-            temp=temp*10+c;
             x=x/10;
+            temp=temp*10+c;
         }
         if(temp>INT_MAX || temp<INT_MIN)
             return 0;
