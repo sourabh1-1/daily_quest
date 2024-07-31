4b20678bd5a5ebcd89a33c987a0ec3fccc034b3d
sourabh1-1 <116716478+sourabh1-1@users.noreply.github.com>
2024-07-31 18:24:40 +0530

    Time: 19 ms (37.13%), Space: 21.9 MB (16.43%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n=nums.size();
+        int i=0;
+        for(int j=0;j<n;j++){
+            if(nums[j]!=0)
+                nums[i++]=nums[j];
+        }
+        while(i<n)
+            nums[i++]=0;
+    }
+};
